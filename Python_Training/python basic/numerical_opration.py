86732c0e2215ebd8b494d688b8564e167dd85cb3
anshul1144 <anshulspandey2004.com>
2026-03-06 11:20:06 +0530

    Fix Python_Training folder upload

---
+++ Python_Training/python basic/numerical_opration.py
@@ -0,0 +1,23 @@
+# Perform numerical operations 
+# initialize two numbers
+num1 = 10
+num2 = 5
+#....................................
+# addition
+print("The addition of", num1, "and", num2, "is:", num1 + num2)
+#Difference
+print("The difference of", num1, "and", num2, "is:", num1 - num2)
+#Multiplication
+print("The multiplication of", num1, "and", num2, "is:", num1 * num2)
+#Division
+print("The division of", num1, "and", num2, "is:", num1 / num2)
+#Modulus
+print("The modulus of", num1, "and", num2, "is:", num1 % num2)
+#.....................................
+
+""" Output:
+The addition of 10 and 5 is: 15
+The difference of 10 and 5 is: 5
+The multiplication of 10 and 5 is: 50
+The division of 10 and 5 is: 2.0
+The modulus of 10 and 5 is: 0"""
