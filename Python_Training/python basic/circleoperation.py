db8e2922c2bf23ad10b13b40139a12fc34c2de6f
anshul1144 <anshulspandey2004.com>
2026-03-06 19:23:27 +0530

    Added circle operation program

--- Python_Training/python basic/circleoperation.py
+++ Python_Training/python basic/circleoperation.py
@@ -1,12 +1,16 @@
 # operatons of circle
 # importing math module
+#.................................................
 import math
 # variable to store radius of circle
 radius= 10
 # calculating area of circle
+#...................................................
 print("Area of the circle is: ", math.pi * radius ** 2)
 # calculating circumference of circle
 print("Circumference of the circle is: ", 2 * math.pi * radius)
+
+#..................................................
 
 """ Output: 
 Area of the circle is:  314.1592653589793
