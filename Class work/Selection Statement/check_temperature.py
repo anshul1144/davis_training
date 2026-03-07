98e928f5a4a7c4c80477a57477520985440118a9
anshul1144 <anshulspandey2004.com>
2026-03-07 10:38:37 +0530

    First comit

---
+++ Class work/Selection Statement/check_temperature.py
@@ -0,0 +1,13 @@
+# program to check the temperature
+# taking input from the user
+temp= int(input("Enter the temperature: "))
+# checking the temperature
+if temp > 30:
+    print("It's a hot day.")
+else:
+    print("It's a cold day.")
+#...................................................
+""" Output:
+Enter the temperature: 35
+It's a hot day.
+"""
