855319839448d5581360aeab5f36f68b31b3f513
anshul1144 <anshulspandey2004.com>
2026-03-07 11:30:03 +0530

    first comit

---
+++ Class work/Selection Statement/Check_Greatestnumber.py
@@ -0,0 +1,31 @@
+# program to check greatest number among five numbers
+
+#taking numbers from the user
+num1= float(input("Enter the first number: "))
+num2= float(input("Enter the second number: "))
+num3= float(input("Enter the third number: "))
+num4= float(input("Enter the fourth number: "))
+num5= float(input("Enter the fifth number: "))
+
+#checking the greatest number among the five numbers
+if num1 >= num2 and num1 >= num3 and num1 >= num4 and num1 >= num5:
+    print(num1, "is the greatest number.")
+elif num2 >= num1 and num2 >= num3 and num2 >= num4 and num2 >= num5:
+    print(num2, "is the greatest number.")
+elif num3 >= num1 and num3 >= num2 and num3 >= num4 and num3 >= num5:
+    print(num3, "is the greatest number.")
+elif num4 >= num1 and num4 >= num2 and num4 >= num3 and num4 >= num5:
+    print(num4, "is the greatest number.")
+else:
+    print(num5, "is the greatest number.")
+    
+#................................................................
+
+""" Output:
+Enter the first number: 10
+Enter the second number: 20
+Enter the third number: 30
+Enter the fourth number: 40
+Enter the fifth number: 50
+50.0 is the greatest number.
+"""
