903913c5bc8dbeefa22fa9d3afddbc89bd9429ff
anshul1144 <anshulspandey2004.com>
2026-03-17 11:05:49 +0530

    first commit

---
+++ Modules/Numeric_calculations/Arthematic_operation.py
@@ -0,0 +1,24 @@
+# Arthematic Operation Module
+
+# functions for basic arithmetic operations such as addition
+def calculate_sum(num1, num2):
+    return num1 + num2
+
+# functions for basic arithmetic operations such as difference
+def calculate_difference(num1, num2):
+    return num1 - num2
+
+# functions for basic arithmetic operations such as product
+
+def calculate_product(num1, num2):
+    return num1 * num2
+
+# functions for basic arithmetic operations such as quotient
+
+def calculate_quotient(num1, num2):
+    return num1 / num2
+
+# functions for basic arithmetic operations such as modulus 
+
+def calculate_modulus(num1, num2):
+    return num1 % num2
