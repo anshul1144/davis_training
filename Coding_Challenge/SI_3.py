2d158d89f06eb4a590d9bc4e572492c21b94225a
anshul1144 <anshulspandey2004.com>
2026-03-18 11:18:33 +0530

    A

---
+++ Coding_Challenge/SI_3.py
@@ -0,0 +1,11 @@
+# Program to calculate SI using map
+# ....................................................................
+p, r, t = map(float, input("Enter P R T: ").split())
+
+print(f"Simple Interest: {(p*r*t)/100}")
+# ....................................................................
+"""
+Output:
+Enter P R T: 1000 5 2
+Simple Interest: 100.0
+"""
