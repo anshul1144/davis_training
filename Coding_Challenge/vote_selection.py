eb787de44d0ee55c85c42bb1aea6d4fee4e62d74
anshul1144 <anshulspandey2004.com>
2026-03-18 11:05:31 +0530

    simple commit message

---
+++ Coding_Challenge/vote_selection.py
@@ -0,0 +1,14 @@
+# Program to check voting (simple if)
+# ....................................................................
+age = int(input("Enter age: "))
+
+if age < 18:
+    print("Not Eligible")
+else:
+    print("Eligible")
+# ....................................................................
+"""
+Output:
+Enter age: 22
+Eligible
+"""
