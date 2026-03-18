b7d07226a659a42c3e2878c2041a14e7850b1c44
anshul1144 <anshulspandey2004.com>
2026-03-18 11:23:13 +0530

    simple message

---
+++ Coding_Challenge/SI_simple.py
@@ -0,0 +1,16 @@
+# Program to calculate SI (simple)
+# ....................................................................
+p = float(input("Enter P: "))
+r = float(input("Enter R: "))
+t = float(input("Enter T: "))
+
+si = (p*r*t)/100
+print(f"Simple Interest: {si}")
+# ....................................................................
+"""
+Output:
+Enter P: 500
+Enter R: 10
+Enter T: 1
+Simple Interest: 50.0
+"""
