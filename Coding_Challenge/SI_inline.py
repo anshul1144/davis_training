f3e7c1db8bccd755494af8cac15c8b74c5478698
anshul1144 <anshulspandey2004.com>
2026-03-18 11:07:49 +0530

    inline code for SI calculation

---
+++ Coding_Challenge/SI_inline.py
@@ -0,0 +1,15 @@
+# Program to calculate simple interest (inline)
+# ....................................................................
+p = float(input("Enter P: "))
+r = float(input("Enter R: "))
+t = float(input("Enter T: "))
+
+print(f"Simple Interest: {(p*r*t)/100}")
+# ....................................................................
+"""
+Output:
+Enter P: 2000
+Enter R: 5
+Enter T: 2
+Simple Interest: 200.0
+"""
