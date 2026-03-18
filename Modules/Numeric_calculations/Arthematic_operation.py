90bfdc3aab2eb9cdef2aa63daf04ec6a35d2b894
anshul1144 <anshulspandey2004.com>
2026-03-18 09:52:11 +0530

    A

--- Modules/Numeric_calculations/Arthematic_operation.py
+++ Modules/Numeric_calculations/Arthematic_operation.py
@@ -1,24 +1,24 @@
 # Arthematic Operation Module
 
-# functions for basic arithmetic operations such as addition
+# functions for basic arthmetic operations such as addition
 def calculate_sum(num1, num2):
     return num1 + num2
 
-# functions for basic arithmetic operations such as difference
+# functions for basic arthmetic operations such as difference
 def calculate_difference(num1, num2):
     return num1 - num2
 
-# functions for basic arithmetic operations such as product
+# functions for basic arthmetic operations such as product
 
 def calculate_product(num1, num2):
     return num1 * num2
 
-# functions for basic arithmetic operations such as quotient
+# functions for basic arthmetic operations such as quotient
 
 def calculate_quotient(num1, num2):
     return num1 / num2
 
-# functions for basic arithmetic operations such as modulus 
+# functions for basic arthmetic operations such as modulus  8
 
 def calculate_modulus(num1, num2):
     return num1 % num2
