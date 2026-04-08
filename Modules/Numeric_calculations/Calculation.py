aa4823d46e6be69cae8b14660faa9f371ebb85fc
anshul1144 <anshulspandey2004.com>
2026-04-08 10:43:47 +0530

    perform numeric calculation by using modules

---
+++ Modules/Numeric_calculations/Calculation.py
@@ -0,0 +1,12 @@
+# program to calculate sum of two numbers using a module
+
+# importing the module for basic arthmetic operations
+import Numeric_calculations.Arthematic_operation
+
+m=10
+n=20
+# display the sum of two numbers
+print("The sum of", m, "and", n, "is:", Numeric_calculations.Arthematic_operation.calculate_sum(m, n))
+
+# Display the product of two numbers
+print("The product of", m, "and", n, "is:", Numeric_calculations.Arthematic_operation.calculate_product(m, n))
