4b468b8eb598a8502dcf7b51b8eb4f194487e052
anshul1144 <anshulspandey2004.com>
2026-04-17 11:24:38 +0530

    program to check the user is eligible for vote or not by using exception handling.

--- Class work/Exception Handling/Vote_eligibility.py
+++ Class work/Exception Handling/Vote_eligibility.py
@@ -1,14 +1,21 @@
 # Write a program to check the user is eligible for vote or not by using exception handling.
 
 #...............................................................................................
+
+# create custom exception class 
+class AgeException(Exception):
+    pass
+
 try:
-    age = int(input("Enter your age: "))
+    age = int(input("Enter your age in year: "))
     if age < 0:
-        raise ValueError("Age cannot be negative.")
+        raise AgeException("Age cannot be negative.")
     elif age < 18:
         print("Error: You are not eligible to vote.")
     else:
         print("You are eligible to vote.")
+except AgeException:
+    print("Error: Age cannot be negative.")
 except ValueError:
     print("Error: Please enter a valid age.")
     
