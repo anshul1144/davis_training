d350cca86a1428fb73a99c1d1d5864bd1b29da12
anshul1144 <anshulspandey2004.com>
2026-04-20 10:36:26 +0530

    create a numpy array from the even factors of a given number

--- Class work/Numpy/Even_number.py
+++ Class work/Numpy/Even_number.py
@@ -1,7 +1,15 @@
 # create a numpy array from the even factors of a given number
 import numpy as np
-number = int(input("Enter a number: "))
-even_factors = [i for i in range(1, number + 1) if number % i == 0 and i % 2 == 0]
-even_factors_array = np.array(even_factors)
-print("Even factors of", number, "are:", even_factors_array)
 
+number = int(input("Enter a number: "))
+
+even_factors = []
+
+for i in range(1, number + 1):
+    if number % i == 0:
+        if i % 2 == 0:
+            even_factors.append(i)
+
+even_factors_array = np.array(even_factors)
+
+print("Even factors are:", even_factors_array)
