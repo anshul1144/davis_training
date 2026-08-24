4f21d28ff20f3026862d202fc903def2b4de380a
Anshul Pandey <anshulspandey2004@gmail.com>
2026-08-24 11:49:10 +0530

    find the largest & smallest element in the list

---
+++ Class work/DSA/P2.py
@@ -0,0 +1,6 @@
+# find the largest & smallest element in the list 
+arr= [15,19,31,11,29]
+largest = max(arr)
+smallest = min(arr)
+print("Largest element is:", largest)
+print("Smallest element is:", smallest)
